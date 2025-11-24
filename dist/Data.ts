2d2c0ca555dfaf71b469d1f38f147f991cf27b23
zohaib ali <zohaib24a@gmail.com>
2025-11-24 22:57:45 +0500

    build fix

--- dist/Data.ts
+++ dist/Data.ts
@@ -2,53 +2,57 @@ import { PiTextAUnderlineBold } from "react-icons/pi";
 import { IoIosColorPalette } from "react-icons/io";
 import { BiSolidBellPlus } from "react-icons/bi";
 import { IoMdArchive } from "react-icons/io";
-import { BsThreeDotsVertical } from "react-icons/bs";
 import { FaImage } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import { MdDeleteSweep } from "react-icons/md";
 
 const IconsArray = [
-        {
-            id: 1,
-            title:"Formatting options",
-            icon: PiTextAUnderlineBold
-        },
-        {
-            title:"Background",
-            id: 2,
-            icon: IoIosColorPalette
-        },
-        {
-title:"Remind me",
-            id: 3,
-            icon: BiSolidBellPlus
-        },
-        {
-            title:"Add image",
-            id: 4,
-            icon: FaImage
-        },
-        {title:"Archive",
-            id: 5,
-            icon: IoMdArchive
-        },
-        {title:"More",
-            id: 6,
-            icon: BsThreeDotsVertical
-        },
+    {
+        id: 1,
+        tooltip: "Formatting options",
+        icon: PiTextAUnderlineBold
+    },
+    {
+        tooltip: "Background",
+        id: 2,
+        icon: IoIosColorPalette
+    },
+    {
+        tooltip: "Remind me",
+        id: 3,
+        icon: BiSolidBellPlus
+    },
+    {
+        tooltip: "Add image",
+        id: 4,
+        icon: FaImage
+    },
+    {
+        tooltip: "Archive",
+        id: 5,
+        icon: IoMdArchive
+    },
+    {
+        tooltip: "Bin",
+        id: 6,
+        icon: MdDeleteForever,
+       
+    },
 
-    ];
+];
 
-    const DeleteIconsArray = [
-        {
-            id: 1,
-            icon: MdDeleteForever
-        },
-        {
-            id: 2,
-            icon: MdDeleteSweep
-        }]
-    
+const DeleteIconsArray = [
+    {
+        id: 1,
+        tooltip: "Delete forever",
+        icon: MdDeleteForever
+    },
+    {
+        id: 2,
+        tooltip: "Restore",
+        icon: MdDeleteSweep
+    }]
 
-    export default IconsArray;
+
+export default IconsArray;
 export { DeleteIconsArray };
